cc8f1c7e4cabdd98ba6f11dc435fe63d746a4ed1
Abele-n <otienoabele89@gmail.com>
2023-11-08 08:10:15 -0800

    Review class that inherit from BaseModel

---
+++ models/review.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The Review class definition"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class represenation
+
+Attributes:
+place_id (str): place id
+user_id(str): user id
+text (str): review text
+"""
+
+
+place_id = ""
+user_id = ""
+text = ""
