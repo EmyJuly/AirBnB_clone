06135bdf4c3db1d5bc68eff5912f3b317a03ffc0
Abele-n <otienoabele89@gmail.com>
2023-11-08 07:48:36 -0800

    City class that inherit from BaseModel

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""City class definition"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """City class representation
+
+Attributes:
+state_id (str): state id
+name(str): city name
+"""
+
+
+state_id = ""
+name = ""
