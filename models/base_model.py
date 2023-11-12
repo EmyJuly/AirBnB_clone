28943b87ba487166688e7eb890858d855ee9f31c
Abele-n <otienoabele89@gmail.com>
2023-11-11 17:05:48 -0800

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -10,53 +10,51 @@ class BaseModel:
 
 
 def save(self):
-    """Keeps updated_at up to date with current datetime"""
-
-
-self.updated_at = datetime.today()
-save.models.storage()
+    """
+    Keeps updated_at up to date with current datetime
+    """
+    self.updated_at = datetime.today()
+    save.models.storage()
 
 
 def __init__(self, *args, **kwargs):
-    """New BaseModel initialization
-Args:
-*args: not used
-**kwargs (dict): key-value pairs of attributes
-"""
+    """
+    New BaseModel initialization
 
-
-self.id = str(uuid4)
-tform = "%Y-%m-%dT%H:%M:%S.%f"
-self.updated_at = datetime.today()
-self.created_at = datetime.today()
-
-if len(kwargs) != 0:
-    for m, n in kwargs.items():
-        if m == "created_at" or m == "updated_at":
-            self.__dict__[m] = datetime.strptime(n, tform)
-        else:
-            self.__dict__[m] = n
-else:
-    new.model.storage(self)
+    Args:
+    *args: not used
+    **kwargs (dict): key-value pairs of attributes
+    """
+    self.id = str(uuid4)
+    tform = "%Y-%m-%dT%H:%M:%S.%f"
+    self.updated_at = datetime.today()
+    self.created_at = datetime.today()
+    if len(kwargs) != 0:
+        for m, n in kwargs.items():
+            if m == "created_at" or m == "updated_at":
+                self.__dict__[m] = datetime.strptime(n, tform)
+            else:
+                self.__dict__[m] = n
+    else:
+        new.model.storage(self)
 
 
 def to_dictionary(self):
-    """The dictionary of the BaseModel instance is returned
-representation of a key-value pair __class__
-included of the class name of the object
-"""
-
-
-copy_dict = self.__dict__.copy()
-copy_dict["created_at"] = self.created_at.isoformat()
-copy_dict["updated_at"] = self.updated_at.isoformat()
-copy_dict["__class__"] = self.__class__.__name__
-return copy_dict
+    """
+    The dictionary of the BaseModel instance is returned
+    representation of a key-value pair __class__
+    included of the class name of the object
+    """
+    copy_dict = self.__dict__.copy()
+    copy_dict["created_at"] = self.created_at.isoformat()
+    copy_dict["updated_at"] = self.updated_at.isoformat()
+    copy_dict["__class__"] = self.__class__.__name__
+    return copy_dict
 
 
 def __str__(self):
-    """The BaseModel instance string representation is returned"""
-
-
-class_name = self.__class__.__name__
-return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+    """
+    The BaseModel instance string representation is returned
+    """
+    class_name = self.__class__.__name__
+    return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,52 +11,48 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Abstract storage engine representation
-Attributes:
-__file_path (str): file name to save objects to
-__objects(dict): Instantiated objects dictionary
-"""
+    """
+    Abstract storage engine representation
 
+    Attributes:
+    __file_path (str): file name to save objects to
+    __objects(dict): Instantiated objects dictionary
+    """
+    __file_path = "file.json"
+    __objects = {}
 
-__file_path = "file.json"
-__objects = {}
+    def all(self):
+        """
+        The dictionary __objects is returned
+        """
+        return FileStorage.__objects
 
+    def save(self):
+        """
+        __objects serialization to JSON file __file_path
+        """
+        objdict = FileStorage.__objects
+        newobjdict = {obj: objdict[obj].to_dict() for obj in objdict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(newobjdict, f)
 
-def all(self):
-    """The dictionary __objects is returned"""
+    def new(self, obj):
+        """
+        __objects is set with <obj_class_name>.id key
+        """
+        objclname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(objclname, obj.id)] = obj
 
-
-return FileStorage.__objects
-
-
-def save(self):
-    """__objects serialization to JSON file __file_path"""
-
-
-objdict = FileStorage.__objects
-newobjdict = {obj: objdict[obj].to_dict() for obj in objdict.keys()}
-with open(FileStorage.__file_path, "w") as f:
-    json.dump(newobjdict, f)
-
-
-def new(self, obj):
-    """__objects is set with <obj_class_name>.id key"""
-
-
-objclname = obj.__class__.__name__
-FileStorage.__objects["{}.{}".format(objclname, obj.id)] = obj
-
-
-def reload(self):
-    """JSON file __file_path deserialized to __objects when it exists"""
-
-
-try:
-    with open(FileStorage.__file_path) as f:
-        objdict = json.load(f)
-for o in objdict.values():
-    class_name = o["__class__"]
-del o["__class__"]
-self.new(eval(cls_name)(**o))
-except FileNotFoundError:
-    return
+    def reload(self):
+        """
+        JSON file __file_path deserialized to __objects when it exists
+        """
+        try:
+            with open(FileStorage.__file_path) as f:
+                newobjdict = json.load(f)
+                for o in newobjdict.values():
+                    class_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
